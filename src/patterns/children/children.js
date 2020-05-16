75a89ff960945796dd12284fd0bc7ee5b566294f
ilia <ilyakut@gmail.com>
2020-05-17 00:02:28 +0300

    added children.js description

--- src/patterns/children/children.js
+++ src/patterns/children/children.js
@@ -8,6 +8,7 @@ const Children = (props) => {
         Через props.children (или this.props.children для класса) мы можем получить доступ<br/>
         к контенту котой был передан внутрь компонента (как слот во Vue)<br/>
         props.children - может быть функцией или любым другим типом данных
+        в React есть набор методов для работы с children - React.Children
       </p>
       Пример: {props.children('children test')}
     </div>
